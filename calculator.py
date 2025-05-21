c2f677ad0110dc01c41cb4d610d4bdc4f5dea1a7
NobleSparky <christopherjmartin10@gmail.com>
2025-05-20 21:42:18 -0500

    Updated calculator "round" command
    I had issues with the "round" command. It is now fixed. It's not perfect but it works finally.

--- calculator.py
+++ calculator.py
@@ -2,6 +2,7 @@
 #A super basic calculator. Nothing special lol
 
 #This is the introduction. Just something extra. I used time.sleep so its not overwhelming while running
+import math 
 import time
 print('This is a basic calculator made by: NobleSparky')
 time.sleep(2)
@@ -12,7 +13,6 @@ time.sleep(2)
 print("Type a math procedure. \nObtions are: \nadd \nsubtract \ndivide \nmultiply \nround \nexit")
 
 #This is the command type system to do basic math operations
-import math
 while True:
     start_math_procedure = input('Enter text >> ')
     if start_math_procedure == 'add':
@@ -36,11 +36,10 @@ while True:
         product_result = num1 * num2
         print('The product is: ', product_result)
     elif start_math_procedure == 'round':
-        print('This feature currently does not work. . .')
-        #num = float(input('Enter your number >> '))
-        #num_int = int(num)
-        #rounded_num = math.floor(num_int)
-        #print('The rounded number is:', math.ceil(num_int))
+        num = float(input('Enter a number >> '))
+        print('When rounding. Each negitive value represents a place \nSo -1 is tens, and etc. . .')
+        round_to = int(input('Enter a number >> '))
+        print(round(num, round_to))
     elif start_math_procedure == 'exit':
         print('Exiting program. . .')
         break
